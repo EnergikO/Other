f1a65fdbad5d6e750640d8585b2d482052e97fa7
Максим <43538054+EnergikO@users.noreply.github.com>
2021-11-02 14:20:22 +0300

    Update work_with_dict.py

--- work_with_dict.py
+++ work_with_dict.py
@@ -19,29 +19,29 @@ def delete_items(_dict: dict, *keys) -> dict:
 	return new_dict
 
 
-def sort_by_keys(_dict: dict, reverse=False) -> list:
-	return sorted(list(_dict.items()), key=lambda x: x[0], reverse=reverse)
+def sort_by_keys(_dict: dict, key=lambda x: x[0], reverse: bool = False) -> list:
+	return sorted(list(_dict.items()), key=key, reverse=reverse)
 
 
-def sort_by_values(_dict: dict, reverse=False) -> list:
-	return sorted(list(_dict.items()), key=lambda x: x[1], reverse=reverse)
+def sort_by_values(_dict: dict, key=lambda x: x[1], reverse: bool = False) -> list:
+	return sorted(list(_dict.items()), key=key, reverse=reverse)
 
 
-def fill_dict_randomly(n, _range):
+def fill_dict_randomly(n: int, _range: [list, set, tuple]) -> dict:
 	from random import randint
-
 	_dict = dict()
+	a, b = _range[0], _range[1]
+
 	for i in range(n):
-		_dict[i] = randint(*_range)
+		_dict[i] = randint(a, b)
 
 	return _dict
 
 
-def print_dict(_dict: dict):
+def print_dict(_dict: [dict, list, set, tuple]) -> None:
 	for elem in _dict:
 		try:
 			print(elem.__repr__())
 
 		except AttributeError:
 			print(elem)
-
